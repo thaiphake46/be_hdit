4262ed365097622cce107f5f281d92d962a5bd94
huy thai <huythaia123@gmail.com>
2023-02-26 11:17:44 +0700

    21 Kết Nối Tới MySQL Database với Sequelize - Render Dữ Liệu Ra View | Cấu hình cho Node.JS phần 3

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,14 @@
 import express from "express"
+import db from '../models/index'
 
-const getHomePage = (req, res) => {
-  res.render('home.ejs')
+const getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll()
+
+    res.render('home.ejs', { data: JSON.stringify(data) })
+  } catch (e) {
+    console.log()
+  }
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from './config/viewEngine'
 import initWebRouter from './route/web'
+import testConnectDB from './config/conenctDB'
+
 require('dotenv').config()
 
 const app = express()
@@ -11,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 viewEngine(app)
 initWebRouter(app)
+testConnectDB()
 
 let port = process.env.PORT
 app.listen(port, () => {
